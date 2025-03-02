efd6acbb8f4f3c58caf415d3913dc5ac419a7886
gilang <156637129+gilang1223@users.noreply.github.com>
2025-03-02 21:56:06 +0700

    penambahan fitur

--- tpmod3/Form1.Designer.cs
+++ tpmod3/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.submit = new System.Windows.Forms.Button();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(572, 74);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 35);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(57, 78);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(459, 26);
+            this.textInput.TabIndex = 1;
+            this.textInput.TextChanged += new System.EventHandler(this.textInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- tpmod3/Form1.cs
+++ tpmod3/Form1.cs
@@ -15,6 +15,23 @@ namespace tpmod3
         public Form1()
         {
             InitializeComponent();
+            label1.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                label1.Visible = true;
+                label1.Text = textInput.Text;
+        }
+
+        private void textInput_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
